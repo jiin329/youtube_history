9bc0df5dee249fb8952180686890b6c3b3c5fed0
jiin <jiin329@gmail.com>
2022-10-29 17:08:28 +0900

    create project

---
+++ front-end/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SignIn from './domain/signIn';
+
+function App() {
+  return (
+      <div>
+        <SignIn />
+      </div>
+  );
+}
+
+export default App;
